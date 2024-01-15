388aadfe36c665e29d74b4bb0808cb6173220c8d
KTC96 <kyleclow96@gmail.com>
2024-01-15 19:27:11 +0000

    configure sw3

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -171,6 +172,26 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    #Bucket config
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-kyle'
+    AWS_S3_REGION_NAME ="eu-west-2"
+    AWS_ACCESS_KEY_ID = os.environ.get(' AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get(' AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    #STatic and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorages'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLS in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
