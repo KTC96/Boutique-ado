b2452d8d8f31ecfd48625d955965aae21fd92f80
KTC96 <kyleclow96@gmail.com>
2024-01-15 17:14:48 +0000

    Change allowed host

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'boutique-ado-kyle.herokuapp.com']
+ALLOWED_HOSTS = ['boutique-ado-kyle-ac7dcd056efb.herokuapp.com', 'localhost']
 
 # Application definition
 
