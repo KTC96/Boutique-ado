cad1688e21760748168aa0d0730de2aa457eb8e4
KTC96 <kyleclow96@gmail.com>
2024-01-18 14:51:22 +0000

    add runtime.txt

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -213,7 +213,6 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 
-EMAIL_KEYFILE = None
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
